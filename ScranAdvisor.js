8cb6a3e354bc342c59be68d074ccadbae0cdb8ff
Danash Mahmood <danash.mahmoodof@gmail.com>
2024-06-19 14:33:45 +0100

    completed mvp

--- ScranAdvisor.js
+++ ScranAdvisor.js
@@ -7,4 +7,38 @@ ScranAdvisor.prototype.numberOfRestaurants = function(){
     return this.restaurants.length;
 }
 
+ScranAdvisor.prototype.findRestaurantByName = function(nameOfRestaurant){
+    for(let restaurant of this.restaurants){
+        if(restaurant.name === nameOfRestaurant){
+            return restaurant;
+        }
+    
+    }
+    return undefined;
+}
+
+ScranAdvisor.prototype.findAllRestaurantNames = function(){
+    restaurantNames = [];
+    this.restaurants.forEach(element => {
+        restaurantNames.push(element.name)
+        
+    });
+
+    return restaurantNames;
+}
+
+
+ScranAdvisor.prototype.findRestaurantByCity = function(cityName){
+    restaurantInCity = [];
+    this.restaurants.forEach(element => {
+        if(element.location.town === cityName){
+            restaurantInCity.push(element);
+        }
+    });
+
+    return restaurantInCity;
+}
+
+
+
 module.exports = ScranAdvisor;
